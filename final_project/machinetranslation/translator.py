59f2ed448541baf3a1dab8c227cffdf108f2717f
jsthill <83320920+jsthill@users.noreply.github.com>
2022-06-08 01:25:36 -0400

    Code added to check for blank text to translate.

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -3,7 +3,6 @@ import os
 from ibm_watson import LanguageTranslatorV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 from dotenv import load_dotenv
-from sqlalchemy import null
 
 load_dotenv()
 
@@ -38,5 +37,3 @@ def french_to_english(frenchtext):
     model_id='fr-en').get_result()
 
     return englishtext.get('translations')[0].get('translation')
-
-# print(english_to_french('Hello, my name is Julian'))    
